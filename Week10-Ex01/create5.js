9a8e3eb1c16bd11611f00f2a4995a1a452e8bb44
grega186 <60275845+grega186@users.noreply.github.com>
2020-03-31 17:20:01 -0400

    Add files via upload

---
+++ Week10-Ex01/create5.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+  $("#button1").click(function() {
+    $("#hats").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button2").click(function() {
+    $("#necklaces").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button3").click(function() {
+    $("#tops").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button4").click(function() {
+    $("#sweaters").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button5").click(function() {
+    $("#jackets").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button6").click(function() {
+    $("#dresses").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button7").click(function() {
+    $("#bags").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button8").click(function() {
+    $("#pants").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button9").click(function() {
+    $("#skirts").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#button10").click(function() {
+    $("#shoes").toggle();
+  });
+});
+$(document).ready(function() {
+  $("#shoes").draggable();
+});
+$(document).ready(function() {
+  $("#skirts").draggable();
+});
+$(document).ready(function() {
+  $("#pants").draggable();
+});
+$(document).ready(function() {
+  $("#bags").draggable();
+});
+$(document).ready(function() {
+  $("#dresses").draggable();
+});
+$(document).ready(function() {
+  $("#jackets").draggable();
+});
+$(document).ready(function() {
+  $("#sweaters").draggable();
+});
+$(document).ready(function() {
+  $("#tops").draggable();
+});
+$(document).ready(function() {
+  $("#necklaces").draggable();
+});
+$(document).ready(function() {
+  $("#hats").draggable();
+});
